006ed56acc6496c45a8fae5e9664313a38a38b3e
KHAKhazeus <khazeus@outlook.com>
2020-05-26 22:23:52 +0800

    try to add UI and health interaction

---
+++ Assets/Scripts/GameLogic/StartSceneController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartSceneController : MonoBehaviour
+{
+    public Button startBtn;
+    // Use this for initialization
+    void Start()
+    {
+        startBtn.onClick.AddListener(StartGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
