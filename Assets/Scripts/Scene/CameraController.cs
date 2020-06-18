4eb1c4aa2446878399ba5bba3293998eb81a2622
enginewang <engine74396@gmail.com>
2020-06-18 19:33:43 +0800

    modify main menu bg&font, add camera rotate(Q&E key)

--- Assets/Scripts/Scene/CameraController.cs
+++ Assets/Scripts/Scene/CameraController.cs
@@ -4,27 +4,47 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-  public Transform PlayerTransform;
-  private Vector3 _cameraOffset;
-
-  [Range(0.01f, 1.0f)]
-  public float SmoothFactor = 0.5f;
-
-  void Start()
-  {
-      _cameraOffset = transform.position - PlayerTransform.position;
-
-  }
-
-  void LateUpdate()
-  {
-      Vector3 newPos = PlayerTransform.position + _cameraOffset;
-      transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
-  }
-
-    // Update is called once per frame
-    void Update()
+    private Vector3 offset;
+ 
+    //-----Publics variables-----\\
+    [Header("Variables")]
+    public Transform player;
+ 
+    [Space]
+    [Header("Position")]
+    public float camPosX;
+    public float camPosY;
+    public float camPosZ;
+ 
+    [Space]
+    [Header("Rotation")]
+    public float camRotationX;
+    public float camRotationY;
+    public float camRotationZ;
+ 
+    [Space]
+    [Range(0f, 10f)]
+    public float turnSpeed;
+ 
+    //-----Privates functions-----\\
+    private void Start()
     {
-
+        offset = new Vector3(camPosX,  camPosY, camPosZ);
+        transform.rotation = Quaternion.Euler(camRotationX, camRotationY, camRotationZ);
+    }
+ 
+ 
+    private void LateUpdate()
+    {
+        //offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) *  offset;
+        if (Input.GetKey(KeyCode.Q))
+        {
+            offset = Quaternion.AngleAxis(turnSpeed, Vector3.up) *  offset;
+        }else if (Input.GetKey(KeyCode.E))
+        {
+            offset = Quaternion.AngleAxis(-turnSpeed, Vector3.up) *  offset;
+        }
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
     }
 }
