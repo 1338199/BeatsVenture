df6e2d1d52e4f7dfc954215bb710ddb5c5833661
冬阳 刘 <392702533@qq.com>
2020-06-22 00:23:36 +0800

    gamePause

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class GameController
+    {
+        static private AudioListener listener;
+        static private void awake()
+        {
+            listener = GameObject.Find("MusicController").GetComponent<AudioListener>();
+        }
+
+        static public bool paused
+        {
+            get;
+            set;
+        } = false;
+
+        static public void pauseGame()
+        {
+            paused = true;
+            AudioListener.pause = true;
+            Time.timeScale = 0;
+        }
+
+        static public void resumeGame()
+        {
+            paused = false;
+            AudioListener.pause = false;
+            Time.timeScale = 1;
+        }
+    }
+}
